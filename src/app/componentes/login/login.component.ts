f8f4ef6e950671199234f4e28f288b3dc367c40d
Eugenio Merino <33074131+Eugenio12345@users.noreply.github.com>
2020-08-26 20:26:17 -0500

    Update login.component.ts

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -38,7 +38,7 @@ export class LoginComponent implements OnInit {
      if(usuario.nombreUsuario==null && usuario.contrasenna==null){
       this.router.navigateByUrl('/login')
      }else if(usuario.nombreUsuario!= null && usuario.contrasenna!=null && usuario.rol=='admin'){
-       console.log('/user-home')
+         this.router.navigateByUrl('/user-home')
      }else{
       this.router.navigateByUrl('/vista-producto')
      }
